21f4b9d84012a03ddff4218cc928458f3dcaedab
Sensokaku <28125632+Sensokaku@users.noreply.github.com>
2025-10-07 14:17:48 +0800

    ops

--- scripts/downloader.js
+++ scripts/downloader.js
@@ -80,7 +80,7 @@ function startdownload(box) {
 	if (box.downloading) {
 		return;
 	}
-	let url = "https://api.nerinyan.moe/d/" + box.sid + "n";
+	let url = "https://api.nerinyan.moe/d/" + box.sid;
 	box.downloading = true;
     box.classList.add("downloading");
     let xhr = new XMLHttpRequest();
