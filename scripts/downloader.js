c9d57680c09b7efca5d55591db7baa3536ebeb04
Sensokaku <28125632+Sensokaku@users.noreply.github.com>
2025-10-07 13:59:29 +0800

    maybe better

--- scripts/downloader.js
+++ scripts/downloader.js
@@ -80,7 +80,7 @@ function startdownload(box) {
 	if (box.downloading) {
 		return;
 	}
-	let url = "https://catboy.best/d/" + box.sid + "n";
+	let url = "https://rina.place/d/" + box.sid + "n";
 	box.downloading = true;
     box.classList.add("downloading");
     let xhr = new XMLHttpRequest();
