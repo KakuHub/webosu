39582e14ed7086440775fc043542d0d845bf484a
Sensokaku <28125632+Sensokaku@users.noreply.github.com>
2025-10-07 14:14:31 +0800

    it is what it is

--- scripts/downloader.js
+++ scripts/downloader.js
@@ -80,7 +80,7 @@ function startdownload(box) {
 	if (box.downloading) {
 		return;
 	}
-	let url = "https://rina.place/d/" + box.sid + "n";
+	let url = "https://api.nerinyan.moe/d/" + box.sid + "n";
 	box.downloading = true;
     box.classList.add("downloading");
     let xhr = new XMLHttpRequest();
